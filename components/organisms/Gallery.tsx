52a9fc9298dc773bb466425295069bc341952490
k_95.21 <kohta0421kochi@gmail.com>
2024-03-29 12:37:22 +0900

    add new organisms components gellery

---
+++ components/organisms/Gallery.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+// scss import
+import styles from "./styles/Gallery.module.scss";
+
+// components import
+
+const Gellery = () => {
+  return (
+    <div className={styles.gellery}>
+      <div className={styles.container}>
+        <h1>gellery area</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Gellery;
